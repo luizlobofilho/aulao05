7abb9cbcba468b9b2cccadf8833a9b825c5399c7
luiz lobo <lp.lobofilho@gmail.com>
2021-12-12 15:59:44 -0300

    Add Product memory

--- src/main/java/com/devsuperior/myfirstproject/entites/Product.java
+++ src/main/java/com/devsuperior/myfirstproject/entites/Product.java
@@ -1,13 +1,9 @@
 package com.devsuperior.myfirstproject.entites;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-public class Category implements Serializable {
+public class Product implements Serializable {
 
     private static final long serialVersionUID;
 
@@ -17,17 +13,18 @@ public class Category implements Serializable {
 
     private Long id;
     private String name;
+    private Double price;
 
-    @JsonIgnore
-    private List<Product> products = new ArrayList<>();
+    private Category category;
 
-    public Category() {
-
+    public Product() {
     }
 
-    public Category(Long id, String name) {
+    public Product(Long id, String name, Double price, Category category) {
         this.id = id;
         this.name = name;
+        this.price = price;
+        this.category = category;
     }
 
     public Long getId() {
@@ -46,16 +43,28 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    public List<Product> getProducts() {
-        return products;
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return id.equals(category.id);
+        Product product = (Product) o;
+        return id.equals(product.id);
     }
 
     @Override
---
+++ src/main/java/com/devsuperior/myfirstproject/repositories/ProductRepository.java
@@ -0,0 +1,27 @@
+package com.devsuperior.myfirstproject.repositories;
+
+import com.devsuperior.myfirstproject.entites.Product;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ProductRepository {
+
+    private final Map<Long, Product> map = new HashMap<>();
+
+    public void save(Product obj){
+        map.put(obj.getId(), obj);
+    }
+
+    public Product findById(Long id){
+        return map.get(id);
+    }
+
+    public List<Product> findAll(){
+        return new ArrayList<Product>(map.values());
+    }
+}
